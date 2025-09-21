9acbfb6d35b694fdcece9d3909caf08248026918
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-21 11:05:08 +0800

    [BUGFIX] animation enhance
    Change-Id: I01cdee8b0def4d67a20f837e4a568df01e222fff

--- quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
+++ quickstep/src/com/android/launcher3/QuickstepTransitionManager.java
@@ -2046,6 +2046,9 @@ public class QuickstepTransitionManager implements OnDeviceProfileChangeListener
             } else {
                 composeIconLaunchAnimator(anim, mV, appTargets, wallpaperTargets, nonAppTargets,
                         launcherClosing);
+                if (mLauncher.getStateManager().getState() == LauncherState.BACKGROUND_APP) {
+                    anim.setDuration(0L);
+                }
                 addCujInstrumentation(anim, Cuj.CUJ_LAUNCHER_APP_LAUNCH_FROM_ICON);
                 skipFirstFrame = false;
             }
