232ddda549f8f47f8a9ecbdd88c9f50ed326d643
Ghosuto <clash.raja10@gmail.com>
2025-10-08 03:24:19 +0000

    Launcher3: Set default recent opacity to 20
    Change-Id: I884c9b115268d0ba867b6693533d418c89be78d1

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -281,7 +281,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val SHOW_STATUS_BAR = backedUpItem("pref_show_statusbar", true)
         @JvmField val SHOW_TOP_SHADOW = backedUpItem("pref_show_top_shadow", true)
         @JvmField val BLUR_DEPTH = backedUpItem("pref_blur_depth", 23)
-        @JvmField val RECENTS_OPACITY = backedUpItem("pref_recents_opacity", 100)
+        @JvmField val RECENTS_OPACITY = backedUpItem("pref_recents_opacity", 20)
         @JvmField val APP_DRAWER_OPACITY = backedUpItem("pref_app_drawer_opacity", 90)
         @JvmField val BLUR_BACKGROUND_AT_APP_LAUNCH = backedUpItem("pref_blur_background_at_app_launch", true)
         @JvmField val SINGLE_PAGE_CENTER = backedUpItem("pref_single_page_center", false)
