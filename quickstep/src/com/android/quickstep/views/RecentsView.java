f35525271ad2de102bc918862c4de709f7ae3304
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-05-31 01:13:26 +0000

    Launcher3: Fix custom recents scale settle animation after dismiss
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -5253,7 +5253,16 @@ public abstract class RecentsView<
                 || mRecentsStyle.equals("oxygen");
     }
 
+    public boolean isKamiRecentsStyleActive() {
+        return mRecentsStyle.equals("staple");
+    }
+
     public float getStackDismissReflowTarget(TaskView taskView, float reflowTarget) {
+        return getStackDismissReflowTarget(taskView, reflowTarget, false);
+    }
+
+    public float getStackDismissReflowTarget(TaskView taskView, float reflowTarget,
+            boolean trailingStackTask) {
         if (!isStackRecentsStyleActive()) {
             return reflowTarget;
         }
@@ -5261,6 +5270,15 @@ public abstract class RecentsView<
         float overlapFactor = getStackStyleOverlapFactor();
         if (overlapFactor <= 0f) {
             return reflowTarget;
+        }
+        if (mRecentsStyle.equals("staple")) {
+            float childSize = getPagedOrientationHandler().getMeasuredSize(taskView);
+            if (childSize == 0f) {
+                return reflowTarget;
+            }
+            float stackTranslation = getStackStyleTranslation(reflowTarget, childSize,
+                    overlapFactor);
+            return reflowTarget + stackTranslation * (trailingStackTask ? 2.35f : 1f);
         }
 
         boolean touchInLandscape = mOrientationState.getTouchRotation() != ROTATION_0
